4b5cc5aaf8e147d25e817774677615a9ba93b085
unknown <dbehera647@gmail.com>
2025-01-04 12:05:22 +0530

    Setting up the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+ 
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 function App() {
    
 
-  return (
-    <>
-       
-      <h1>Vite + React</h1>
-       
+  return (<>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Body/>}>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+
+    </Route>
+    
+     
+    </Routes></BrowserRouter>
+  
     </>
+    
+    
+ 
   )
 }
 
